568d8266bd957bd0c3a56d83158ff6fe3e295971
riana <rianaandrianome@gmail.com>
2026-02-22 15:49:44 +0300

    specify build port

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, '.', '');
+  return {
+    plugins: [react()],
+    server: {
+      host: true,
+      port: 8056,
+      strictPort: true,
+      proxy: {
+        '/api': { target: 'http://localhost:8056', changeOrigin: true },
+      },
+    },
+  };
 });
